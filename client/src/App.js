a332263833fe875e05fe8115314b282fc19b333a
KarLOL82 <klinfeldt@gmail.com>
2022-11-05 14:19:20 -0700

    added connections page

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,38 @@
-import React from 'react';
+import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
-import './App.css';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import "./App.css";
 // import { Input } from "@material-tailwind/react";
 
-
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import ConnectWith from "./pages/ConnectWith";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -52,27 +51,12 @@ function App() {
           <Header />
           <div className="container justify-center">
             <Routes>
-              <Route 
-                path="/"
-                element={<Home />}
-              />
-              <Route 
-                path="/login"
-                element={<Login />}
-              />
-              <Route 
-                path="/signup"
-                element={<Signup />}
-              />
-              <Route 
-                path="/me"
-                element={<Profile />}
-              />
-              <Route 
-                path="/profiles/:username"
-                element={<Profile />}
-              />
-              
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/me" element={<Profile />} />
+              <Route path="/ConnectWith" element={<ConnectWith />} />
+              <Route path="/profiles/:username" element={<Profile />} />
             </Routes>
           </div>
           <Footer />
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ConnectWith from "../../pages/ConnectWith";
 
 import Auth from "../../utils/auth";
 
@@ -11,18 +12,27 @@ const Header = () => {
   return (
     <header className="bg-yellow-500 p-10  text-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-      <div>
+        <div>
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn text-gray-700 btn-lg btn-info m-2" to="/me">
+              <Link
+                className="btn text-xl text-gray-700 float-right font-bold btn-lg btn-light m-2"
+                to="/me"
+              >
                 {Auth.getProfile().data.username}'s profile
               </Link>
               <button
-                className="btn text-gray-700 btn-lg btn-light m-2"
+                className="btn text-xl text-gray-700 float-right font-bold btn-lg btn-light m-2"
                 onClick={logout}
               >
                 Logout
               </button>
+              <Link
+                className="btn text-xl text-gray-700 float-right font-bold btn-lg btn-light m-2"
+                to="/ConnectWith"
+              >
+                {ConnectWith}Find Connections
+              </Link>
             </>
           ) : (
             <>
@@ -41,16 +51,15 @@ const Header = () => {
             </>
           )}
         </div>
-        
+
         <div>
-          <Link className="text-gray-900 text-4xl font-bold" to="/">
+          <Link className="text-gray-900 py-10 text-4xl font-bold" to="/">
             <h1 className="m-0 py-10 ">Clever Music App Name</h1>
           </Link>
           <p className="m-0 text-xl py-6 text-gray-700">
             Connect With Others Through Music
           </p>
         </div>
-        
       </div>
     </header>
   );
