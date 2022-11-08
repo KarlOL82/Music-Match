6ef17019d9c042f07c14b9b393b85fee1e3f9e2d
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-07 21:42:33 -0800

    Got data to save to local Storage, along with the buttons being operational

--- client/src/pages/ProfileCreator.js
+++ client/src/pages/ProfileCreator.js
@@ -1,207 +1,230 @@
-// import React, { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
+// import React from "react";
 
 //******************* NOTES **********************/
 // Currently working on adding this info to local storage
 // Need to update models to have genres, aboutMe, Name, Birthday(if we want), and each role(Musician, Artist, Producer)
 // CSS still need to be done but it has a basic setup for now.
 // ************************************************/
-const profileCreator = () => {
+const ProfileCreator = () => {
+
+    const [userData, setUserData] = useState({
+        _id: "",
+        name: "",
+        // dob_day:"",
+        // dob_month:"",
+        // dob_year:"",
+        display_role: false,
+        role: "",
+        role_interest:"",
+        url:"",
+        about_me:"",
+        matches: []
+    })
+
   const handleSubmit = (e) => {
     console.log("it works");
   };
-  const handleChange = () => {
+  const handleChange = (e) => {
     console.log("it worked too");
+    const value = e.target.type === "checkedbox" ? e.target.checked : e.target.value
+    const name = e.target.name
+    console.log("value " + value);
+    
+
+    setUserData((prevState) => ({
+        ...prevState,
+        [name] : value
+    }))
+    console.log(userData);
   };
 
   return (
     <>
-      <div className="flex-row justify-center">
-        <h1>Edit Profile</h1>
+        <h1 className="flex justify-center">Edit Profile</h1>
 
-        <form onSubmit={handleSubmit}></form>
-        <section>
-          {/* This will be where cloudnairy button to upload photo will go  */}
-          <div className="flex justify-end">
-          <label className="uploadProfile">Upload photo</label>
-          <input
-            type="url"
-            name="url"
-            id="url"
-            onChange={handleChange}
-            // this way the user doesnt have to put a photo in
-            required={false}
-          />
-          </div>
-        </section>
-
-        <section>
-          {/* This div is for Tailwind edits */}
-          <div className="field col-12 col-md-10 mb-3 p-3">
-            <label className="first_name">What do you go by? </label>
-            <input
-              id="first_name"
-              type="text"
-              name="first_name"
-              placeholder="First Name"
-              required={true}
-              value={""}
-              onChange={handleChange}
-            />
-          </div>
-          {/* this container will collect all of the birthday digits */}
-          <label>Birthday </label>
-          <div className="birthday-container">
-            <div className="field col-12 col-md-10 mb-3 p-3">
-              <input
-                id="dob_day"
-                type="number"
-                name="dob_day"
-                placeholder="DD"
-                required={true}
-                value={""}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="field col-12 col-md-10 mb-3 p-3">
-              <input
-                id="dob_month"
-                type="number"
-                name="dob_month"
-                placeholder="MM"
-                required={true}
-                value={""}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="field col-12 col-md-10 mb-3 p-3">
-              <input
-                id="dob_year"
-                type="number"
-                name="dob_year"
-                placeholder="YYYY"
-                required={true}
-                value={""}
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-
-          {/* this will allow the user to select if they are a artist, musician, or producer */}
-          <label>Roles</label>
-          <div className="role-container">
+      <div className="flex justify-center">
+        <form onSubmit={handleSubmit}>
+          <section >
             {/* This div is for Tailwind edits */}
             <div className="field col-12 col-md-10 mb-3 p-3">
+              <label className="name">What do you go by? </label>
               <input
-                id="artist-role"
-                type="radio"
-                name="artist"
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Name"
                 required={true}
-                value={"artist"}
+                value={userData.name}
                 onChange={handleChange}
-                // this will leave the box un-selected
-                checked={false}
               />
-              <label className="artist"> Artist </label>
             </div>
-            <div className="field col-12 col-md-10 mb-3 p-3">
-              <input
-                id="musician-role"
-                type="radio"
-                name="musician"
-                required={true}
-                value={"musician"}
-                onChange={handleChange}
-                // this will leave the box un-selected
-                checked={false}
-              />
-              <label className="musician"> Musician </label>
-            </div>
-            <div className="field col-12 col-md-10 mb-3 p-3">
-              <input
-                id="producer-role"
-                type="radio"
-                name="producer"
-                required={true}
-                value={"producer"}
-                onChange={handleChange}
-                // this will leave the box un-selected
-                checked={false}
-              />
-              <label className="producer"> Producer </label>
-            </div>
-          </div>
-          {/* this is a checkbox to display role tag */}
-          <div className="field col-12 col-md-10 mb-3 p-3">
-            <label className="display-role ">Display Role Tag</label>
-            <input
-              id="display-role"
-              type="checkbox"
-              name="display-role"
-              onChange={handleChange}
-              // this will leave the box un-selected
-              checked={false}
-            />
-          </div>
-          {/* This will be for picking what intrest they have */}
-          <label>What Do You Want To See?</label>
-          <div className="field col-12 col-md-10 mb-3 p-3">
-            <div className="role-interest-container">
-              <input
-                id="artist-interest"
-                type="radio"
-                name="role-interest"
-                required={true}
-                value={"artist"}
-                onChange={handleChange}
-                // this will leave the box un-selected
-                checked={false}
-              />
-              <label className="artist"> Artist </label>
-              <input
-                id="musician-interest"
-                type="radio"
-                name="role-interest"
-                required={true}
-                value={"musician"}
-                onChange={handleChange}
-                // this will leave the box un-selected
-                checked={false}
-              />
-              <label className="musician"> Musician </label>
+            {/* this container will collect all of the birthday digits */}
+            {/* <label>Birthday </label>
+            <div className="birthday-container">
+              <div className="field col-12 col-md-10 mb-3 p-3">
+                <input
+                  id="dob_day"
+                  type="number"
+                  name="dob_day"
+                  placeholder="DD"
+                  required={true}
+                  value={""}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="field col-12 col-md-10 mb-3 p-3">
+                <input
+                  id="dob_month"
+                  type="number"
+                  name="dob_month"
+                  placeholder="MM"
+                  required={true}
+                  value={""}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="field col-12 col-md-10 mb-3 p-3">
+                <input
+                  id="dob_year"
+                  type="number"
+                  name="dob_year"
+                  placeholder="YYYY"
+                  required={true}
+                  value={""}
+                  onChange={handleChange}
+                />
+              </div>
+            </div> */}
 
+            {/* this will allow the user to select if they are a artist, musician, or producer */}
+            <label>Roles</label>
+            <div className="role-container">
+              {/* This div is for Tailwind edits */}
+              <div className="field col-12 col-md-10 mb-3 p-3">
+                <input
+                  id="artist_role"
+                  type="radio"
+                  name="role"
+                  required={true}
+                  value={"artist"}
+                  onChange={handleChange}
+                  // this will leave the box un-selected
+                  checked={userData.role === "artist"}
+                />
+                <label className="artist"> Artist </label>
+              </div>
+              <div className="field col-12 col-md-10 mb-3 p-3">
+                <input
+                  id="musician_role"
+                  type="radio"
+                  name="role"
+                  required={true}
+                  value={"musician"}
+                  onChange={handleChange}
+                  // this will leave the box un-selected
+                  checked={userData.role === "musician"}
+                />
+                <label className="musician"> Musician </label>
+              </div>
+              <div className="field col-12 col-md-10 mb-3 p-3">
+                <input
+                  id="producer_role"
+                  type="radio"
+                  name="role"
+                  required={true}
+                  value={"producer"}
+                  onChange={handleChange}
+                  // this will leave the box un-selected
+                  checked={userData.role === "producer"}
+                />
+                <label className="producer"> Producer </label>
+              </div>
+            </div>
+            {/* this is a checkbox to display role tag */}
+            <div className="field col-12 col-md-10 mb-3 p-3">
+              <label className="display-role ">Display Role Tag</label>
               <input
-                id="producer-interest"
-                type="radio"
-                name="role-interest"
-                required={true}
-                value={"producer"}
+                id="display_role"
+                type="checkbox"
+                name="display_role"
                 onChange={handleChange}
                 // this will leave the box un-selected
-                checked={false}
+                checked={userData.display_role}
               />
-              <label className="producer"> Producer</label>
             </div>
+            {/* This will be for picking what intrest they have */}
+            <label>What Do You Want To See?</label>
+            <div className="field col-12 col-md-10 mb-3 p-3">
+              <div className="role-interest-container">
+                <input
+                  id="artist_interest"
+                  type="radio"
+                  name="role_interest"
+                  required={true}
+                  value={"artist"}
+                  onChange={handleChange}
+                  // this will leave the box un-selected
+                  checked={userData.role_interest === "artist"}
+                />
+                <label className="artist"> Artist </label>
+                <input
+                  id="musician_interest"
+                  type="radio"
+                  name="role_interest"
+                  required={true}
+                  value={"musician"}
+                  onChange={handleChange}
+                  // this will leave the box un-selected
+                  checked={userData.role_interest === "musician"}
+                />
+                <label className="musician"> Musician </label>
 
-            {/* label for about me  */}
-            <label className="about-me">About me </label>
-            <input
-              id="about"
-              type="text"
-              name="about-me"
-              required={true}
-              placeholder="Tell me about yourself"
-              value={""}
-              onChange={handleChange}
-            />
-          </div>
-          <input type="submit" />
-        </section>
-       
-      
+                <input
+                  id="producer_interest"
+                  type="radio"
+                  name="role_interest"
+                  required={true}
+                  value={"producer"}
+                  onChange={handleChange}
+                  // this will leave the box un-selected
+                  checked={userData.role_interest === "producer"}
+                />
+                <label className="producer"> Producer</label>
+              </div>
+
+              {/* label for about me  */}
+              <label className="about-me">About me </label>
+              <input
+                id="about"
+                type="text"
+                name="about_me"
+                required={true}
+                placeholder="Tell me about yourself"
+                value={userData.about_me}
+                onChange={handleChange}
+              />
+            </div>
+            <input type="submit" />
+          </section>
+          <section>
+            {/* This will be where cloudnairy button to upload photo will go  */}
+            
+              <label className="uploadProfile">Upload photo</label>
+              <input
+                type="url"
+                name="url"
+                id="url"
+                onChange={handleChange}
+                // this way the user doesnt have to put a photo in
+                required={false}
+              />
+           
+          </section>
+        </form>
       </div>
     </>
   );
 };
 
-export default profileCreator;
+export default ProfileCreator;
