2c1b9468543540300acb93811baf829799568ebc
KarLOL82 <klinfeldt@gmail.com>
2022-11-07 19:53:31 -0800

    rendering basic profiles to connection page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,8 +13,8 @@ const Home = () => {
     <main>
       <div className="flex-row justify-center">
         <div
-          className="bg-yellow-500 justify-center opacity-70 col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
+          className="bg-yellow-500 justify-center text-center opacity-70 col-12 col-md-10 mb-3 p-3"
+          
         >
           More words
         </div>
