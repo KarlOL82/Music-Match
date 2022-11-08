058fba9fb991d0f43733faa428815532f097df34
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-07 19:20:22 -0800

    finished form for editing profile page

--- client/src/pages/ProfileCreator.js
+++ client/src/pages/ProfileCreator.js
@@ -11,13 +11,14 @@ const profileCreator = () => {
 
   return (
     <>
-      <div className="test">
+      <div className="flex-row justify-center">
         <h2>Edit Profile</h2>
 
         <form onSubmit={handleSubmit}></form>
         <section>
-        <div className="field col-12 col-md-10 mb-3 p-3">
-            <label className="first_name">First Name </label>
+          {/* This div is for Tailwind edits */}
+          <div className="field col-12 col-md-10 mb-3 p-3">
+            <label className="first_name">What do you go by? </label>
             <input
               id="first_name"
               type="text"
@@ -28,93 +29,155 @@ const profileCreator = () => {
               onChange={handleChange}
             />
           </div>
-
-          <label>Birthday </label>
           {/* this container will collect all of the birthday digits */}
+          <label>Birthday </label>
           <div className="birthday-container">
             <div className="field col-12 col-md-10 mb-3 p-3">
-            <input
-              id="dob_day"
-              type="number"
-              name="dob_day"
-              placeholder="DD"
-              required={true}
-              value={""}
-              onChange={handleChange}
-            />
+              <input
+                id="dob_day"
+                type="number"
+                name="dob_day"
+                placeholder="DD"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
             </div>
             <div className="field col-12 col-md-10 mb-3 p-3">
-            <input
-              id="dob_month"
-              type="number"
-              name="dob_month"
-              placeholder="MM"
-              required={true}
-              value={""}
-              onChange={handleChange}
-            />
+              <input
+                id="dob_month"
+                type="number"
+                name="dob_month"
+                placeholder="MM"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
             </div>
             <div className="field col-12 col-md-10 mb-3 p-3">
-            <input
-              id="dob_year"
-              type="number"
-              name="dob_year"
-              placeholder="YYYY"
-              required={true}
-              value={""}
-              onChange={handleChange}
-            />
+              <input
+                id="dob_year"
+                type="number"
+                name="dob_year"
+                placeholder="YYYY"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
             </div>
           </div>
-          <label>Role</label>
+
           {/* this will allow the user to select if they are a artist, musician, or producer */}
+          <label>Roles</label>
           <div className="role-container">
-            <input
-              id="artist"
-              type="radio"
-              name="artist"
-              required={true}
-              value={"artist"}
-              onChange={handleChange}
-              // this will leave the box un-selected
-              checked={false}
-            />
-            <label className="artist">Artist</label>
-
-            <input
-              id="musician"
-              type="radio"
-              name="musician"
-              required={true}
-              value={"musician"}
-              onChange={handleChange}
-              // this will leave the box un-selected
-              checked={false}
-            />
-            <label className="musician">Musician</label>
-
-            <input
-              id="producer"
-              type="radio"
-              name="producer"
-              required={true}
-              value={"producer"}
-              onChange={handleChange}
-              // this will leave the box un-selected
-              checked={false}
-            />
-            <label className="producer">Producer</label>
+            {/* This div is for Tailwind edits */}
+            <div className="field col-12 col-md-10 mb-3 p-3">
+              <input
+                id="artist-role"
+                type="radio"
+                name="artist"
+                required={true}
+                value={"artist"}
+                onChange={handleChange}
+                // this will leave the box un-selected
+                checked={false}
+              />
+              <label className="artist"> Artist </label>
+            </div>
+            <div className="field col-12 col-md-10 mb-3 p-3">
+              <input
+                id="musician-role"
+                type="radio"
+                name="musician"
+                required={true}
+                value={"musician"}
+                onChange={handleChange}
+                // this will leave the box un-selected
+                checked={false}
+              />
+              <label className="musician"> Musician </label>
+            </div>
+            <div className="field col-12 col-md-10 mb-3 p-3">
+              <input
+                id="producer-role"
+                type="radio"
+                name="producer"
+                required={true}
+                value={"producer"}
+                onChange={handleChange}
+                // this will leave the box un-selected
+                checked={false}
+              />
+              <label className="producer"> Producer </label>
+            </div>
           </div>
-          <label className="display-role">Display Role Tag</label>
-          <input
-            id="display-role"
-            type="checkbox"
-            name="display-role"
-            onChange={handleChange}
-            // this will leave the box un-selected
-            checked={false}
-          />
+          {/* this is a checkbox to display role tag */}
+          <div className="field col-12 col-md-10 mb-3 p-3">
+            <label className="display-role ">Display Role Tag</label>
+            <input
+              id="display-role"
+              type="checkbox"
+              name="display-role"
+              onChange={handleChange}
+              // this will leave the box un-selected
+              checked={false}
+            />
+          </div>
+          {/* This will be for picking what intrest they have */}
+          <label>What Do You Want To See?</label>
+          <div className="field col-12 col-md-10 mb-3 p-3">
+            <div className="role-interest-container">
+              <input
+                id="artist-interest"
+                type="radio"
+                name="role-interest"
+                required={true}
+                value={"artist"}
+                onChange={handleChange}
+                // this will leave the box un-selected
+                checked={false}
+              />
+              <label className="artist"> Artist </label>
+              <input
+                id="musician-interest"
+                type="radio"
+                name="role-interest"
+                required={true}
+                value={"musician"}
+                onChange={handleChange}
+                // this will leave the box un-selected
+                checked={false}
+              />
+              <label className="musician"> Musician </label>
+
+              <input
+                id="producer-interest"
+                type="radio"
+                name="role-interest"
+                required={true}
+                value={"producer"}
+                onChange={handleChange}
+                // this will leave the box un-selected
+                checked={false}
+              />
+              <label className="producer"> Producer</label>
+            </div>
+
+            {/* label for about me  */}
+            <label className="about-me">About me </label>
+            <input
+              id="about"
+              type="text"
+              name="about-me"
+              required="true"
+              placeholder="Tell me about yourself"
+              value={""}
+              onchange={handleChange}
+            />
+          </div>
+          <input type="submit" />
         </section>
+      
       </div>
     </>
   );
