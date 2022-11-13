bb87f89a364710bdabe30d09e2c750d02fba7d16
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-12 19:25:53 -0800

    fixed the signup page to match theme of our other pages

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -39,11 +39,14 @@ const Signup = () => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
+    <main className="flex justify-center  pt-12 mb-4">
+      <div class="bg"></div>
+      <div class="bg bg2"></div>
+      <div class="bg bg3"></div>
       <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
-          <div className="card-body">
+        <div className="card bg-slate-300 p-3 text-gray-900 rounded-lg">
+          <h4 className="card-header flex justify-center  font-bold bg-dark text-light p-2">Sign Up</h4>
+          <div className="">
             {data ? (
               <p>
                 Success! You may now head{' '}
@@ -76,7 +79,7 @@ const Signup = () => {
                   onChange={handleChange}
                 />
                 <button
-                  className="btn btn-block btn-primary"
+                  className="btn btn-block btn-primary px-1 font-bold rounded-lg hover:bg-slate-600"
                   style={{ cursor: 'pointer' }}
                   type="submit"
                 >
