3c5788b36e4e8cdad959a16e4a1130456e65e3cb
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-12 19:59:25 -0800

    Fixed the formating for profileCreator and profile files, along with adding the update profile button inside of the container with the rest of the data

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const Signup = () => {
   const [formState, setFormState] = useState({
-    username: '',
-    email: '',
-    password: '',
+    username: "",
+    email: "",
+    password: "",
   });
   const [addUser, { error, data }] = useMutation(ADD_USER);
 
@@ -45,11 +45,13 @@ const Signup = () => {
       <div class="bg bg3"></div>
       <div className="col-12 col-lg-10">
         <div className="card bg-slate-300 p-3 text-gray-900 rounded-lg">
-          <h4 className="card-header flex justify-center  font-bold bg-dark text-light p-2">Sign Up</h4>
+          <h4 className="card-header flex justify-center  font-bold bg-dark text-light p-2">
+            Sign Up
+          </h4>
           <div className="">
             {data ? (
               <p>
-                Success! You may now head{' '}
+                Success! You may now head{" "}
                 <Link to="/">back to the homepage.</Link>
               </p>
             ) : (
@@ -80,7 +82,7 @@ const Signup = () => {
                 />
                 <button
                   className="btn btn-block btn-primary px-1 font-bold rounded-lg hover:bg-slate-600"
-                  style={{ cursor: 'pointer' }}
+                  style={{ cursor: "pointer" }}
                   type="submit"
                 >
                   Submit
